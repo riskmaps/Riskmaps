4cd87f9b63e16be38fed86c766a7729ad66328f9
M3lissaHernandez <soymelissahernandez@gmail.com>
2025-04-25 23:55:46 -0500

    Configuracion de urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@
 URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#mysite/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from myapp.views import map_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', map_view, name='map_root'),
+    #path('', home_view, name='home'),  # Página principal
+    path('', include('myapp.urls')),   # Incluye las URLs de la app
+    
 ]
